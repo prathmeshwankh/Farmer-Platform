35b5d1ed6edee85a0924fd6849100b7cdcef5488
Prathmesh wankhade <prathmeshwankhade15@gmail.com>
2026-04-10 01:06:45 +0530

    Add HTML structure to farmers.php

--- farmers.php
+++ farmers.php
@@ -1,10 +1,12 @@
 <?php include 'db.php'; ?>
+
 <!DOCTYPE html>
 <html>
 <head>
 <title>Farmers List</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body class="p-4">
 
 <h2 class="text-center">Farmers Registered</h2>
