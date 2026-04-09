1caafb5c1c5dcb1962540503065f124bba48418a
Prathmesh wankhade <prathmeshwankhade15@gmail.com>
2026-04-10 02:10:22 +0530

    Add product insertion process in add_product_process.php

--- add_product_process.php
+++ add_product_process.php
@@ -1,1 +1,15 @@
+<?php
+include 'db.php';
 
+$name = $_POST['farmer_name'];
+$product = $_POST['product_name'];
+$type = $_POST['type'];
+$price = $_POST['price'];
+$desc = $_POST['description'];
+$unit = $_POST['unit'];
+
+mysqli_query($conn, "INSERT INTO products (farmer_name, product_name, type, price, unit, description)
+VALUES ('$name', '$product', '$type', '$price', '$unit', '$desc')");
+
+echo "<script>alert('Product Added'); window.location='index.php';</script>";
+?>
