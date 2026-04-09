40a8c653d9dbabddf52c06849877b4278653d50e
Prathmesh wankhade <prathmeshwankhade15@gmail.com>
2026-04-10 01:17:16 +0530

    Add feedback submission functionality

--- feeback.php
+++ feeback.php
@@ -1,1 +1,10 @@
+<?php
+include 'db.php';
 
+$name = $_POST['name'];
+$msg = $_POST['message'];
+
+mysqli_query($conn, "INSERT INTO feedback (name, message) VALUES ('$name','$msg')");
+
+echo "<script>alert('Thanks for feedback!'); window.location='index.php';</script>";
+?>
