5cf2554bd620ac100603e6e73b51fb5107edc98a
Prathmesh wankhade <prathmeshwankhade15@gmail.com>
2026-04-09 23:25:19 +0530

    Fix formatting in farmers.php

--- farmers.php
+++ farmers.php
@@ -5,7 +5,6 @@
 <title>Farmers List</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-
 <body class="p-4">
 
 <h2 class="text-center">Farmers Registered</h2>
