deec2ab3af5229c2880b423a104d24fd470453ac
Prathmesh wankhade <prathmeshwankhade15@gmail.com>
2026-04-10 01:36:17 +0530

    Implement add to cart functionality

--- add_to_cart.php
+++ add_to_cart.php
@@ -1,1 +1,19 @@
+<?php
+include 'db.php';
 
+$product = $_POST['product_name'];
+$price = $_POST['price'];
+
+// Check if product already in cart
+$check = mysqli_query($conn, "SELECT * FROM cart WHERE product_name='$product'");
+
+if(mysqli_num_rows($check) > 0){
+    mysqli_query($conn, "UPDATE cart SET quantity = quantity + 1 WHERE product_name='$product'");
+} else {
+    mysqli_query($conn, "INSERT INTO cart (product_name, price) VALUES ('$product', '$price')");
+}
+
+// Redirect to basket page
+header("Location: cart.php");
+exit();
+?>
